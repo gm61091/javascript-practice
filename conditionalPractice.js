ae26ece3b3a18efc599e8ee3ac23ee9c2ae0fda9
gary merriman <g.merriman691@gmail.com>
2024-02-17 17:47:16 -0600

    saved functions

--- conditionalPractice.js
+++ conditionalPractice.js
@@ -2,76 +2,88 @@
 
 // Write a function that takes a number as an argument and returns "positive" if it's greater than zero, "negative" if it's less than zero, and "zero" if it's equal to zero, using a ternary operator.
 
-// function numCheck(number) {
-//     // let number = 7; didnt work?
+function numCheck(number) {
+    // let number = 7; didnt work?
  
-//      const result = number > 0 ? 'possitive number' : number < 0 ? 'negative number' : 'zero';
-//      return result;
-//  }
- //console.log(numCheck(8));
- //console.log(numCheck(-7));
- //console.log(numCheck(0))
+     const result = number > 0 ? 'possitive number' : number < 0 ? 'negative number' : 'zero';
+     return result;
+ }
+ console.log(numCheck(8));
+ console.log(numCheck(-7));
+ console.log(numCheck(0))
  //questions - why no parenthesis?
 
 
 // Create a function that takes three numbers as arguments and returns the largest number, using an if/else statement.
 
-// function largestNumber(num1, num2, num3) {
-//     if (num1 >= num2 && num1 >= num3) {
-//         return num1;
-//     }
-//     else if (num2 >= num1 && num2 >= num3) {
-//         return num2;
-//     }
-//     else {
-//         return num3;
-//     }
-// }
-// console.log(largestNumber(81, 73, 90));
-// console.log(largestNumber(13, 45, 78));
-// console.log(largestNumber(19, 20, 11));
+function largestNumber(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+console.log(largestNumber(81, 73, 90));
+console.log(largestNumber(13, 45, 78));
+console.log(largestNumber(19, 20, 11));
 // // no questions
 
 // Write a function that takes a string as an argument and returns "uppercase" if the string is in all uppercase letters, "lowercase" if it's in all lowercase letters, and "mixed case" if it's a mix of uppercase and lowercase letters, using if/else if/else statements.
 
-// function isCaps(string) {
-//     if (string === string.toUpperCase()) {
-//         return 'ALL CAPS';
-//     }
-//     else if (string === string.toLowerCase()) {
-//         return 'no caps';
-//     }
-//     else {
-//         return 'MiXeD';
-//     }
-// }
-// console.log(isCaps('heLlo'));
-// console.log(isCaps('hola'));
-// console.log(isCaps('HELLO'));
+function isCaps(string) {
+    if (string === string.toUpperCase()) {
+        return 'ALL CAPS';
+    }
+    else if (string === string.toLowerCase()) {
+        return 'no caps';
+    }
+    else {
+        return 'MiXeD';
+    }
+}
+console.log(isCaps('heLlo'));
+console.log(isCaps('hola'));
+console.log(isCaps('HELLO'));
 //no questions
 
 // Create a function that takes an array of numbers and returns a new array containing only the even numbers, using a ternary operator.
+//dez code 
+function namehere (result, num) => {
+return num % 2 === 0 ? result.concat(num) : result, []
+}
+ function evenNum(array) {
+     return array.reduce((result, num) => num % 2 === 0 ? result.concat(num) : result, []);
+}
 
-// function evenNum(array) {
-//     return array.reduce((result, num) => num % 2 === 0 ? result.concat(num) : result, []);
-// }
-// console.log(evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]));
+
+function evenNum(array){
+    let newArr = [];
+    for(let i = 0; i < array.length; i++){
+    array[i] % 2 === 0 ? newArr.push(array[i]) : null
+    }
+    return newArr
+    }
+console.log(evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]));
 //questions - many
 
 // Write a function that takes a number as an argument and returns "fizz" if the number is divisible by 3, "buzz" if it's divisible by 5, and "fizzbuzz" if it's divisible by both 3 and 5, using a ternary operator.
 
-// function buzzyBee(number) {
-//     return number % 3 === 0 ? number % 5 === 0 ? //why no &&
-//     'fizzbuzz' 
-//     : 'fizz' 
-//     : number % 5 === 0 
-//     ? 'buzz'
-//     : number
-// }
-// console.log(buzzyBee(12));
-// console.log(buzzyBee(150));
-// console.log(buzzyBee(125));
-// console.log(buzzyBee(1));
+function buzzyBee(number) {
+    return number % 3 === 0 ? number % 5 === 0 ? 
+    'fizzbuzz' 
+    : 'fizz' 
+    : number % 5 === 0 
+    ? 'buzz'
+    : number
+}
+console.log(buzzyBee(12));
+console.log(buzzyBee(150));
+console.log(buzzyBee(125));
+console.log(buzzyBee(1));
 //questions - finally worked after i removed the end semi colon.
 
 // Create a function that takes a string as an argument and returns "palindrome" if the string is a palindrome (reads the same forwards and backwards), and "not a palindrome" otherwise, using an if/else statement.
