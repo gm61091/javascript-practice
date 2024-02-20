265995d5d896f72c50dc91ded093e5de4d53fcb7
gary merriman <g.merriman691@gmail.com>
2024-02-20 12:37:12 -0600

    arrays done

--- conditionalPractice.js
+++ conditionalPractice.js
@@ -1,58 +1,58 @@
-// // #####$#JavaScript If/Else Statements and Ternary Operators
+// #####$#JavaScript If/Else Statements and Ternary Operators
 
-// // Write a function that takes a number as an argument and returns "positive" if it's greater than zero, "negative" if it's less than zero, and "zero" if it's equal to zero, using a ternary operator.
+// Write a function that takes a number as an argument and returns "positive" if it's greater than zero, "negative" if it's less than zero, and "zero" if it's equal to zero, using a ternary operator.
 
-// function numCheck(number) {
-//     // let number = 7; didnt work?
+function numCheck(number) {
+    // let number = 7; didnt work?
  
-//      const result = number > 0 ? 'possitive number' : number < 0 ? 'negative number' : 'zero';
-//      return result;
-//  }
-//  console.log(numCheck(8));
-//  console.log(numCheck(-7));
-//  console.log(numCheck(0))
-//  //questions - why no parenthesis?
+     const result = number > 0 ? 'possitive number' : number < 0 ? 'negative number' : 'zero';
+     return result;
+ }
+ console.log(numCheck(8));
+ console.log(numCheck(-7));
+ console.log(numCheck(0))
+ //questions - why no parenthesis?
 
 
-// // Create a function that takes three numbers as arguments and returns the largest number, using an if/else statement.
+// Create a function that takes three numbers as arguments and returns the largest number, using an if/else statement.
 
-// function largestNumber(num1, num2, num3) {
-//     if (num1 >= num2 && num1 >= num3) {
-//         return num1;
-//     }
-//     else if (num2 >= num1 && num2 >= num3) {
-//         return num2;
-//     }
-//     else {
-//         return num3;
-//     }
-// }
-// console.log(largestNumber(81, 73, 90));
-// console.log(largestNumber(13, 45, 78));
-// console.log(largestNumber(19, 20, 11));
-// // // no questions
+function largestNumber(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+console.log(largestNumber(81, 73, 90));
+console.log(largestNumber(13, 45, 78));
+console.log(largestNumber(19, 20, 11));
+// // no questions
 
-// // Write a function that takes a string as an argument and returns "uppercase" if the string is in all uppercase letters, "lowercase" if it's in all lowercase letters, and "mixed case" if it's a mix of uppercase and lowercase letters, using if/else if/else statements.
+// Write a function that takes a string as an argument and returns "uppercase" if the string is in all uppercase letters, "lowercase" if it's in all lowercase letters, and "mixed case" if it's a mix of uppercase and lowercase letters, using if/else if/else statements.
 
-// function isCaps(string) {
-//     if (string === string.toUpperCase()) {
-//         return 'ALL CAPS';
-//     }
-//     else if (string === string.toLowerCase()) {
-//         return 'no caps';
-//     }
-//     else {
-//         return 'MiXeD';
-//     }
-// }
-// console.log(isCaps('heLlo'));
-// console.log(isCaps('hola'));
-// console.log(isCaps('HELLO'));
-// //no questions
-// //
+function isCaps(string) {
+    if (string === string.toUpperCase()) {
+        return 'ALL CAPS';
+    }
+    else if (string === string.toLowerCase()) {
+        return 'no caps';
+    }
+    else {
+        return 'MiXeD';
+    }
+}
+console.log(isCaps('heLlo'));
+console.log(isCaps('hola'));
+console.log(isCaps('HELLO'));
+//no questions
+//
 
-// // Create a function that takes an array of numbers and returns a new array containing only the even numbers, using a ternary operator.
-// //dez code 
+// Create a function that takes an array of numbers and returns a new array containing only the even numbers, using a ternary operator.
+//dez code 
 // function namehere (result, num) => {
 // return num % 2 === 0 ? result.concat(num) : result, []
 // }
@@ -61,76 +61,76 @@
 // }
 
 
-// function evenNum(array){
-//     let newArr = [];
-//     for(let i = 0; i < array.length; i++){
-//     array[i] % 2 === 0 ? newArr.push(array[i]) : null
-//     }
-//     return newArr
-//     }
-// console.log(evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]));
-// //questions - many
+function evenNum(array){
+    let newArr = [];
+    for(let i = 0; i < array.length; i++){
+    array[i] % 2 === 0 ? newArr.push(array[i]) : null
+    }
+    return newArr
+    }
+console.log(evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]));
+//questions - many
 
-// // Write a function that takes a number as an argument and returns "fizz" if the number is divisible by 3, "buzz" if it's divisible by 5, and "fizzbuzz" if it's divisible by both 3 and 5, using a ternary operator.
+// Write a function that takes a number as an argument and returns "fizz" if the number is divisible by 3, "buzz" if it's divisible by 5, and "fizzbuzz" if it's divisible by both 3 and 5, using a ternary operator.
 
-// function buzzyBee(number) {
-//     return number % 3 === 0 ? number % 5 === 0 ? 
-//     'fizzbuzz' 
-//     : 'fizz' 
-//     : number % 5 === 0 
-//     ? 'buzz'
-//     : number
-// }
-// console.log(buzzyBee(12));
-// console.log(buzzyBee(150));
-// console.log(buzzyBee(125));
-// console.log(buzzyBee(1));
-// //questions - finally worked after i removed the end semi colon.
+function buzzyBee(number) {
+    return number % 3 === 0 ? number % 5 === 0 ? 
+    'fizzbuzz' 
+    : 'fizz' 
+    : number % 5 === 0 
+    ? 'buzz'
+    : number
+}
+console.log(buzzyBee(12));
+console.log(buzzyBee(150));
+console.log(buzzyBee(125));
+console.log(buzzyBee(1));
+//questions - finally worked after i removed the end semi colon.
 
-// // Create a function that takes a string as an argument and returns "palindrome" if the string is a palindrome (reads the same forwards and backwards), and "not a palindrome" otherwise, using an if/else statement.
+// Create a function that takes a string as an argument and returns "palindrome" if the string is a palindrome (reads the same forwards and backwards), and "not a palindrome" otherwise, using an if/else statement.
 
-// function isSame(string) {
-//     if(string === string.split('').reverse().join('')) {
-//         return 'palindrome';
-//     }
-//     else {
-//         return 'not a palindrome';
-//     }
-// }
-// console.log(isSame('radar'));
-// console.log(isSame('radaar'));
+function isSame(string) {
+    if(string === string.split('').reverse().join('')) {
+        return 'palindrome';
+    }
+    else {
+        return 'not a palindrome';
+    }
+}
+console.log(isSame('radar'));
+console.log(isSame('radaar'));
 
-// // Write a function that takes two numbers as arguments and returns the sum of the square of those numbers, using a ternary operator.
+// Write a function that takes two numbers as arguments and returns the sum of the square of those numbers, using a ternary operator.
 
-// function squareNum(num1, num2) {
-//     return num1 >= 0 && num2 >= 0 ? num1 ** 2 + num2 ** 2 : "negatives";
-// }
-// console.log(squareNum(56, 90));
-// console.log(squareNum(0, 17));
-// console.log(squareNum(56, -3));
-// //questions - why did the && worl here and not the other
+function squareNum(num1, num2) {
+    return num1 >= 0 && num2 >= 0 ? num1 ** 2 + num2 ** 2 : "negatives";
+}
+console.log(squareNum(56, 90));
+console.log(squareNum(0, 17));
+console.log(squareNum(56, -3));
+//questions - why did the && worl here and not the other
 
-// // Create a function that takes an array of strings and returns a new array with only the strings that have a length of at least 5 characters, using a ternary operator.
+// Create a function that takes an array of strings and returns a new array with only the strings that have a length of at least 5 characters, using a ternary operator.
 
-// function isTooLong(array) {
-//     return array.reduce((result, string) => string.length >= 5 ? result.concat(string) : result, []);
-// }
-// console.log(isTooLong(['shshsh', 'testest', 'wow']));
-// //questions = need more understanding on result, []
+function isTooLong(array) {
+    return array.reduce((result, string) => string.length >= 5 ? result.concat(string) : result, []);
+}
+console.log(isTooLong(['shshsh', 'testest', 'wow']));
+//questions = need more understanding on result, []
 
-// // Write a function that takes a number as an argument and returns "even" if the number is even and greater than 10, "odd" if it's odd and greater than 10, and "invalid" if it's less than or equal to 10, using a ternary operator.
+// Write a function that takes a number as an argument and returns "even" if the number is even and greater than 10, "odd" if it's odd and greater than 10, and "invalid" if it's less than or equal to 10, using a ternary operator.
 
-// function numCheck(number) {
-//     return number >= 10 ?
-//     number % 2 === 0 ?
-//     'even and over 10' : 
-//     'odd and over ten' : 
-//     'not over ten';
-// }
-// console.log(numCheck(19));
-// console.log(numCheck(10));
-// console.log(numCheck(9));
-// // no questions
+function numCheck(number) {
+    return number >= 10 ?
+    number % 2 === 0 ?
+    'even and over 10' : 
+    'odd and over ten' : 
+    'not over ten';
+}
+console.log(numCheck(19));
+console.log(numCheck(10));
+console.log(numCheck(9));
+// no questions
 
 // Create a function that takes a string as an argument and returns the first character of the string in uppercase, using a ternary operator.
 
--- loops.js
+++ loops.js
@@ -1,25 +1,29 @@
 //######  methods:
-
+push
 // push(element1, ..., elementN): Adds one or more elements to the end of an array and returns the new length.
 // push() method adds a new element to an array (at the end):
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.push("Kiwi");
 
+pop
 // pop(): Removes the last element from an array and returns that element.
 // pop() method removes the last element from an array:
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.pop();
 
+unshift
 // unshift(element1, ..., elementN): Adds one or more elements to the beginning of an array and returns the new length.
 // unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements:
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.unshift("Lemon");
 
+shift
 // shift(): Removes the first element from an array and returns that element.
 // shift() method removes the first array element and "shifts" all other elements to a lower index.
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.shift();
 
+concat
 // concat(array2, ..., arrayN): Combines two or more arrays.
 // concat() method creates a new array by merging (concatenating) existing arrays:
 const myGirls = ["Cecilie", "Lone"];
@@ -27,27 +31,32 @@ const myBoys = ["Emil", "Tobias", "Linus"];
 
 const myChildren = myGirls.concat(myBoys);
 
+slice
 // slice(start, end): Returns a shallow copy of a portion of an array from the specified start to the specified end.
 // slice() method slices out a piece of an array into a new array:
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 const citrus = fruits.slice(1);
 
+splice
 // splice(start, deleteCount, item1, ..., itemN): Changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
 // splice() method can be used to add new items to an array:
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.splice(2, 0, "Lemon", "Kiwi");
 
+indexOf
 // indexOf(searchElement, fromIndex): Returns the first index at which a given element can be found in the array, or -1 if it is not present.
 // indexOf() method searches an array for an element value and returns its position.
 const fruits = ["Apple", "Orange", "Apple", "Mango"];
 let position = fruits.indexOf("Apple") + 1;
 
+includes
 // includes(element, fromIndex): Determines whether an array includes a certain element, returning true or false as appropriate.
 // ECMAScript 2016 introduced Array.includes() to arrays. This allows us to check if an element is present in an array (including NaN, unlike indexOf).
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 fruits.includes("Mango"); // is true
 
+forEach
 // forEach(callback(currentValue, index, array), thisArg): Executes a provided function once for each array element.
 // forEach() method calls a function (a callback function) once for each array element.
 const numbers = [45, 4, 9, 16, 25];
@@ -58,6 +67,7 @@ function myFunction(value, index, array) {
   txt += value + "<br>";
 }
 
+map
 // map(callback(currentValue, index, array), thisArg): Creates a new array with the results of calling a provided function on every element in the array.
 // map() method creates a new array by performing a function on each array element.
 // map() method does not execute the function for array elements without values.
@@ -69,6 +79,7 @@ function myFunction(value, index, array) {
   return value * 2;
 }
 
+filter
 // filter(callback(currentValue, index, array), thisArg): Creates a new array with all elements that pass the test implemented by the provided function.
 // filter() method creates a new array with array elements that pass a test.
 const numbers = [45, 4, 9, 16, 25];
@@ -78,6 +89,7 @@ function myFunction(value, index, array) {
   return value > 18;
 }
 
+reduce
 // reduce(callback(accumulator, currentValue, index, array), initialValue): Applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value.
 // reduce() method runs a function on each array element to produce (reduce it to) a single value.
 // reduce() method works from left-to-right in the array. See also reduceRight().
@@ -88,10 +100,12 @@ function myFunction(total, value, index, array) {
   return total + value;
 }
 
+reverse
 // reverse(): Reverses the elements of an array in place.
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.reverse()
 
+sort
 // sort(compareFunction): Sorts the elements of an array in place.
 // sort() method sorts an array alphabetically
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
